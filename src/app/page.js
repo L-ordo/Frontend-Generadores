f37c5e4935d607ae1cbde8ae95a957c6337c8235
L-ordonez10 <luis.pedro@bold.gt>
2024-08-25 17:01:29 -0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import mapUnits from './mapUnits';
 
 export default function Home() {
     const [data, setData] = useState(null);
-    const [selectedUnit, setSelectedUnit] = useState('termicas');
+    const [selectedUnit, setSelectedUnit] = useState('hidroelectricas');
 
     useEffect(() => {
         GetOfData(uuidv4()).then((val) => {
@@ -25,10 +25,10 @@ export default function Home() {
     };
 
     const { pie, pie2, unit } = data || {};
-
+    console.log(pie);
     const transformedUnits = mapUnits(unit);
     const filteredUnits = transformedUnits.filter((unit) => unit.type === selectedUnit);
-    // console.log(filteredUnits)
+    console.log(filteredUnits);
 
     const seriesData = filteredUnits.map((unit) => unit.value); 
     const labelsData = filteredUnits.map((unit) => unit.name);
@@ -37,50 +37,52 @@ export default function Home() {
 
     return (
         <>
+            <header className="bg-primary text-white py-3">
+                <div className="container">
+                    <h1 className="text-center">Centro Nacional de Despacho</h1>
+                </div>
+            </header>
 
-    <header className="bg-primary text-white py-3">
-      <div className="container">
-        <h1 className="text-center">Centro Nacional de Despacho</h1>
-      </div>
-    </header>
-
-      <div className="m-5 row">
-            <div className="col-md-6">
-                <Pie
-                    series={pie2}
-                    type={'donut'}
-                />
+            <div className="m-5 row">
+                <div className="col-md-6">
+                    <h2 className="text-center">Energía Renovable vs. Energía Térmica (MW)</h2>
+                    <Pie
+                        series={pie2}
+                        type={'donut'}
+                    />
+                </div>
+                <div className="col-md-6">
+                    <h2 className="text-center">Tipos de Generación (MW)</h2>
+                    <Pie
+                        series={pie}
+                        type={'pie'}
+                    />
+                </div>
             </div>
-            <div className="col-md-6">
-                <Pie
-                    series={pie}
-                    type={'pie'}
-                />
+
+            {/* <label htmlFor="unit">Generador: </label> */}
+            <select
+                className="form-select form-select-lg mb-5 w-25" 
+                aria-label="Large select example"
+                name="unit" 
+                id="unit"
+                onChange={(e) => setSelectedUnit(e.target.value)}
+            >
+                <option value="hidroelectricas">Hidroeléctricas</option>
+                <option value="termicas">Térmicas</option>
+                <option value="solares">Solares</option>
+                <option value="eolicas">Eólicas</option>
+            </select>
+
+            <Pie2
+                series={seriesData}
+                labels={labelsData} 
+                type={'bar'}
+            />
+
+            <div className="text-center mb-5">
+                <Button onClick={refreshData} className="btn-lg">ACTUALIZAR GRÁFICAS</Button>
             </div>
-        </div>
-
-        {/* <label htmlFor="unit">Generador: </label> */}
-        <select
-        class="form-select form-select-lg mb-5 w-25" aria-label="Large select example"
-        name="unit" 
-        id="unit"
-         onChange={(e) => setSelectedUnit(e.target.value)}>
-           {/* <option selected>Generador</option> */}
-            <option value="termicas">Térmicas</option>
-            <option value="hidroelectricas">Hidroeléctricas</option>
-            <option value="solares">Solares</option>
-            <option value="eolicas">Eólicas</option>
-        </select>
-
-      
-        <Pie2
-            series={seriesData}
-            labels={labelsData} 
-            type={'bar'}
-        />
-
-        <Button onClick={refreshData}>ACTUALIZAR GRÁFICAS</Button>
-            
         </>
     );
 }
